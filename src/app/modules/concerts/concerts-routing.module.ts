9d582fc1fd8e4dc62c2cfd2ee72d03b1601881d1
Teodor Sava <savateodor@yahoo.co.uk>
2018-05-14 19:26:38 +0200

    Add concert details and routing

--- src/app/modules/concerts/concerts-routing.module.ts
+++ src/app/modules/concerts/concerts-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ConcertsComponent} from './components/concerts/concerts.component';
+import {ConcertDetailsComponent} from './components/concert-details/concert-details.component';
 
 const routes: Routes = [
     {
         path: '',
         component: ConcertsComponent
+    },
+    {
+        path: 'id/:id',
+        component: ConcertDetailsComponent
     }
 ];
 
--- src/app/modules/concerts/concerts.module.ts
+++ src/app/modules/concerts/concerts.module.ts
@@ -4,13 +4,14 @@ import {CommonModule} from '@angular/common';
 import {ConcertsRoutingModule} from './concerts-routing.module';
 import {ConcertsComponent} from './components/concerts/concerts.component';
 import {ConcertsService} from './services/concerts.service';
+import { ConcertDetailsComponent } from './components/concert-details/concert-details.component';
 
 @NgModule({
     imports: [
         CommonModule,
         ConcertsRoutingModule
     ],
-    declarations: [ConcertsComponent],
+    declarations: [ConcertsComponent, ConcertDetailsComponent],
     providers: [ConcertsService]
 })
 export class ConcertsModule {
