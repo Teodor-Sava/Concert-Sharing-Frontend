36b711b26ba13554afe6dfc97a3a772b9d59122f
Teodor Sava <savateodor@yahoo.co.uk>
2018-05-14 17:49:47 +0200

    Create logic for registering and loging in

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import {AuthenticationRoutingModule} from './authentication-routing.module';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AuthenticationService} from './services/authentication.service';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    AuthenticationRoutingModule
-  ],
-  declarations: [LoginComponent, RegisterComponent]
+    imports: [
+        CommonModule,
+        AuthenticationRoutingModule,
+        ReactiveFormsModule
+    ],
+    declarations: [LoginComponent, RegisterComponent],
+    providers: [AuthenticationService]
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {
+}
--- src/app/modules/authentication/login/login.component.ts
+++ src/app/modules/authentication/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../services/authentication.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
 
-  constructor() { }
+    constructor(private authService: AuthenticationService, private fb: FormBuilder) {
+        this.createForm();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    createForm() {
+        this.loginForm = this.fb.group({
+            email: new FormControl('', Validators.required),
+            password: new FormControl('', Validators.required)
+        });
+    }
+
+    onSubmit(credentials: { email: string, password: string }) {
+        this.authService.login(credentials).subscribe(response => {
+            console.log(response);
+        });
+    }
 }
