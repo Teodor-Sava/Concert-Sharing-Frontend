958a23f4078adbebf20de94e0507da68e253b01e
Teodor Sava <savateodor@yahoo.co.uk>
2018-05-15 19:32:56 +0200

    Design Concert page and concert-details. Add bands module

---
+++ src/app/modules/bands/bands.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BandsRoutingModule } from './bands-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BandsRoutingModule
+  ],
+  declarations: []
+})
+export class BandsModule { }
