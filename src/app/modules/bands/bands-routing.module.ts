698601baa1bf4662affa864b5593c3eb8945f810
Teodor Sava <savateodorbv@gmail.com>
2018-05-20 15:21:11 +0200

    add routing for bands

--- src/app/modules/bands/bands-routing.module.ts
+++ src/app/modules/bands/bands-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {BandsComponent} from './components/bands/bands.component';
+import {BandsDetailsComponent} from './components/bands-details/bands-details.component';
+import {CreateBandComponent} from './components/create-band/create-band.component';
+import {EditBandComponent} from './components/edit-band/edit-band.component';
+import {StatusConcertComponent} from '../concerts/components/status-concert/status-concert.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: BandsComponent
+    },
+    {
+        path: 'id/:id',
+        component: BandsDetailsComponent
+    },
+    {
+        path: 'new',
+        component: CreateBandComponent
+    },
+    {
+        path: 'edit/id/:id',
+        component: EditBandComponent
+    },
+    {
+        path: 'status/id/:id',
+        component: StatusConcertComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class BandsRoutingModule { }
+export class BandsRoutingModule {
+}
