e6a09e12e52ea9c08e8296d187361a74eae3f5ff
Teodor Sava <savateodorbv@gmail.com>
2018-05-20 15:16:49 +0200

    added band services

--- src/app/modules/bands/bands.module.ts
+++ src/app/modules/bands/bands.module.ts
@@ -2,11 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {BandsRoutingModule} from './bands-routing.module';
-import {BandsComponent} from './bands/bands.component';
-import {BandsDetailsComponent} from './bands-details/bands-details.component';
-import {CreateBandComponent} from './create-band/create-band.component';
-import {EditBandComponent} from './edit-band/edit-band.component';
-import {BandConcertStatusComponent} from './band-concert-status/band-concert-status.component';
+import {BandsComponent} from './components/bands/bands.component';
+import {BandsDetailsComponent} from './components/bands-details/bands-details.component';
+import {CreateBandComponent} from './components/create-band/create-band.component';
+import {EditBandComponent} from './components/edit-band/edit-band.component';
+import {BandConcertStatusComponent} from './components/band-concert-status/band-concert-status.component';
 
 @NgModule({
     imports: [
